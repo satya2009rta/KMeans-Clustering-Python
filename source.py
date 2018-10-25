703a96bf6a6e340552abd7f156df71cbaadf8ee7
Arpan Biswas <arpanbiswas97@gmail.com>
2018-10-25 12:20:56 +0530

    Import numpy, pandas and KMeans and add main method

---
+++ source.py
@@ -0,0 +1,14 @@
+# DMML Assignment 2 by Arpan Biswas (BMC201604) and Satya Prakash Nayak (BMC201624)
+# Importing the required packages
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+
+#np.set_printoptions(threshold=np.nan)
+
+# Main Method
+def main():
+
+# Calling main function
+if __name__=="__main__":
+    main()
