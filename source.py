d33bcf33e393cd58c440635290d2fb2acd021dba
Arpan Biswas <arpanbiswas97@gmail.com>
2018-10-26 11:15:38 +0530

    Remove pandas and sklearn packages from import

--- source.py
+++ source.py
@@ -1,8 +1,6 @@
 # DMML Assignment 2 by Arpan Biswas (BMC201604) and Satya Prakash Nayak (BMC201624)
 # Importing the required packages
 import numpy as np
-import pandas as pd
-from sklearn.cluster import KMeans
 
 #np.set_printoptions(threshold=np.nan)
 
