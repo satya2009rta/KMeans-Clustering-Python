8be34ee7fdb1f7ac294f451cd23b2a65b26789c1
Satya Prakash Nayak <satyaprakash2009rta@gmail.com>
2018-11-01 14:06:54 +0530

    Add jaccardDistance, findCentroid and kmeans functions to calculate kmeans for jaccardDistance. (Code not yet tested)

--- source.py
+++ source.py
@@ -1,8 +1,55 @@
 # DMML Assignment 2 by Arpan Biswas (BMC201604) and Satya Prakash Nayak (BMC201624)
 # Importing the required packages
 import numpy as np
+import sys
 
 #np.set_printoptions(threshold=np.nan)
+
+
+def jaccardDistance(doc1, doc2):
+    sameWords = set(doc1).intersection(set(doc2))
+    differentWords = set(doc1).symmetric_difference(set(b))
+    dist = 1 - (len(sameWords)/(len(sameWords) + len(differentWords)))
+    return dist
+
+def findCentroid(internal_cluster, docTerm):
+    new_seeds = []
+    for i, docList in internal_cluster.items():
+        min_sum = sys.maxsize
+        id = 0
+        for j in range(len(docList)):
+            sum_distance = 0
+            for k in range(len(docList)):
+                sum_distance += jaccardDistance(docTerm[docList[j]], docTerm[docList[k]])
+            if(min_sum > sum_distance):
+                min_sum = sum_distance
+                id = docList[j]
+        new_seeds.append(id)
+    return new_seeds
+            
+
+# Calculate K means
+def kmeans(k, given_seeds, docTerm):
+    while(True):
+        internal_cluster = {}
+        for j in range(k):
+            internal_cluster[given_seeds[j]] = []
+        for i in range(len(docTerm)):
+            minimum = sys.maxsize
+            id = 0
+            for j in range(k):
+                dist = jaccardDistance(docTerm[given_seeds[j]], docTerm[i])
+                if(dist < minimum):
+                    minimum = dist
+                    id = given_seeds[j]
+            internal_cluster[id].append(i)
+        new_seeds = []
+        new_seeds = find_centroid(internal_cluster, docTerm)
+        if(jaccardDistance(new_seeds, given_seeds)==0):
+            break
+        given_seeds = new_seeds
+    return new_seeds
+
 
 # Main Method
 def main():
